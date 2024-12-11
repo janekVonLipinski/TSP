3345145c7f480bb787e3fd34c66bad5d935addb9
Max Nolting <max.nolting@viscom.de>
2024-12-11 11:20:47 +0100

    added chainPath method to Path class

--- src/main/java/tsp/model/Path.java
+++ src/main/java/tsp/model/Path.java
@@ -9,4 +9,10 @@ public class Path {
 
     private final List<Integer> path;
     private final int cost;
+
+    public Path chainPath(Path nextPath){
+        path.addAll(nextPath.path);
+        int nextCost = cost + nextPath.cost;
+        return new Path(path, nextCost);
+    }
 }
