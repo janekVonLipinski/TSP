fe15f29bca64992285802b7eba73c1e2aef255d7
Max Nolting <max.nolting@viscom.de>
2025-01-08 09:24:05 +0100

    fixed some warnings

--- src/main/java/tsp/controller/ControllerImpl.java
+++ src/main/java/tsp/controller/ControllerImpl.java
@@ -3,7 +3,6 @@ package tsp.controller;
 import tsp.model.AdjacencyMatrix;
 import tsp.model.Path;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,7 +15,7 @@ public class ControllerImpl implements Controller{
     private List<List<Integer>> getIterations(int numbersUpTo){
 
         if (numbersUpTo == 0) {
-            List<Integer> zeroList = new ArrayList<Integer>();
+            List<Integer> zeroList = new ArrayList<>();
             zeroList.add(0);
             List<List<Integer>> toReturn = new ArrayList<List<Integer>>();
             toReturn.add(zeroList);
@@ -26,7 +25,7 @@ public class ControllerImpl implements Controller{
         List<List<Integer>> newIterations = new ArrayList<List<Integer>>();
         for (List<Integer> iteration : priorIterations){
             for (int i = 0; i < numbersUpTo; i++){
-                List<Integer> tempOld = new ArrayList<Integer>(iteration);
+                List<Integer> tempOld = new ArrayList<>(iteration);
                 tempOld.add(i, numbersUpTo);
                 newIterations.add(tempOld);
             }
