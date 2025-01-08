58663404ebdc2124bf875d9bfbb14d05063e1853
janekVonLipinski <janekvonlipinski222@gmail.com>
2025-01-08 11:11:50 +0100

    added additional test for Dijkstra

--- src/test/java/tsp/controller/dijkstra/DijkstraImplTest.java
+++ src/test/java/tsp/controller/dijkstra/DijkstraImplTest.java
@@ -24,8 +24,27 @@ class DijkstraImplTest {
 
         Path result = dijkstra.calculateShortestPathBetweenToNodes(adjacencyMatrix, startPoint,endPoint);
 
-        System.out.println("correct " + expectedPath);
-        System.out.println("result " + result.getPath());
+        assertArrayEquals(expectedPath.toArray(), result.getPath().toArray());
+    }
+
+    @Test
+    void GIVEN_sixDimensionalMatrix_Then_shortestPathIs_A_B_E_F() {
+
+        int[][] matrix  = {
+                {0, 1, 1, 0, 0, 0},
+                {1, 0, 2, 5, 3, 0},
+                {1, 2, 0, 6, 0, 12},
+                {0, 5, 6, 0, 7, 0},
+                {0, 3, 0, 7, 0, 3},
+                {0, 0, 12, 0, 3, 0}
+        };
+        AdjacencyMatrix adjacencyMatrix = new AdjacencyMatrix(matrix);
+        int startPoint = 0;
+        int endPoint = 5;
+
+        List<Integer> expectedPath = List.of(0, 1, 4, 5);
+
+        Path result = dijkstra.calculateShortestPathBetweenToNodes(adjacencyMatrix, startPoint,endPoint);
 
         assertArrayEquals(expectedPath.toArray(), result.getPath().toArray());
     }
