e7f58a7a75332a271effc4c932118cf576f15ed0
Dennis Berner <dennis.berner@vcat.de>
2025-01-10 08:27:21 +0100

    Added example matrix when no args are given

--- src/main/java/tsp/Main.java
+++ src/main/java/tsp/Main.java
@@ -7,16 +7,22 @@ import tsp.model.InputReader;
 import tsp.model.Path;
 
 import javax.swing.*;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class Main {
 
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("java -jar <filepath> <Optional: Starting Point>");
+        AdjacencyMatrix adjacencyMatrix;
+        if (args.length > 0 && Objects.equals(args[0], "help")) {
+            System.out.println("java -jar <filepath> [Starting Point]");
             return;
         }
-
-        AdjacencyMatrix adjacencyMatrix = InputReader.readFile(args[0]);
+        if (args.length == 0) {
+            adjacencyMatrix = InputReader.readFile("src/main/resources/example_adjacency_matrix.txt");
+        } else {
+            adjacencyMatrix = InputReader.readFile(args[0]);
+        }
 
         Controller controller = new ControllerImpl();
 
